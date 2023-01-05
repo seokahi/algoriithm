aa68e7ff8c80cf9522aeafcfa1eb7533633dc37c
seokahi <skh9797@naver.com>
2023-01-05 20:21:37 +0900

    알고리즘 - 큐,스택

--- PYTHON/programmers/queue_stack/3.py
+++ PYTHON/programmers/queue_stack/3.py
@@ -1,22 +1,26 @@
-stack = []
-
-
-def solution(s):
-    for i in s:
-        if i =='(':
-            stack.append('(')
+priorities = [1, 1, 9, 1, 1, 1]
+location = 0
+def solution(priorities, location):
+    from collections import deque
+    priorities = deque(priorities)
+    process = deque()
+    result = deque()
+    for i in range(0,len(priorities)):
+        process.append(chr(65+i))
+    while len(process)!=0:
+        if len(process) == 1:
+            result.append(process[0])
+            priorities.popleft()
+            process.popleft()
+            break
+        if priorities[0] >= max(list(priorities)[1:]):
+            result.append(process[0])
+            priorities.popleft()
+            process.popleft()
+            
         else:
-            if(len(stack) ==0):
-                return False
-            if(stack[-1] == '('):
-                stack.pop()
-            else:
-                stack.append(')')
-    if len(stack) !=0 :
-        return False
-    return True
+            priorities.append(priorities.popleft())
+            process.append(process.popleft())     
+    return result.index(chr(65+location))+1
 
-print(solution("()()"))
-print(solution("(())()"))
-print(solution(")()("))
-print(solution("(()("))
+print(solution(priorities,location))
