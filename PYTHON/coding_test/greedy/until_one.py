21f460c1849ff7bacf322c5e33d6de983372bf99
seokahi <skh9797@naver.com>
2023-01-07 20:21:37 +0900

    알고리즘 - 그리디

--- PYTHON/coding_test/greedy/until_one.py
+++ PYTHON/coding_test/greedy/until_one.py
@@ -1,24 +1,13 @@
-n,k = map(int,input().split())
+n,k=  map(int,input().split())
 cnt = 0
-# while 1:
-#     while n%k ==0:
-#         n = int(n/k)
-#         cnt +=1
-#         if(n==1):
-#             break
-#     if n ==1:
-#         break
-#     n -= 1
-#     cnt +=1
-# print(cnt)
-while 1:
-    target = int(n/k) *k
-    cnt += (n-target)
-    n = target
-    if(n<k):
-        break
-    cnt +=1
+while n>=k:
+    while n%k !=0:
+        n-=1
+        cnt +=1
     n//=k
+    cnt +=1
+while n!=1:
+    n-=1
+    cnt +=1
 
-cnt += n-1
 print(cnt)
