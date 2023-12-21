edf83a4649be6e8cdefb2c86d22c7c5792e0da3f
seokahi <skh9797@naver.com>
2023-12-21 16:37:04 +0900

    코드업 51~60

---
+++ basic_100/6053.py
@@ -0,0 +1,4 @@
+a=int(input())
+if(bool(a) == False):
+    print("True")
+else: print("False")
---
+++ basic_100/6057.py
@@ -0,0 +1,4 @@
+a,b=input().split()
+if(bool(int(a)) == bool(int(b)) ):
+    print("True")
+else: print("False")
